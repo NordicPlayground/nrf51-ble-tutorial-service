70c3cbc5102219054e683f20f925434fa8992a86
Martin Børs-Lind <martin.bors-lind@nordicsemi.no>
2015-08-10 14:21:01 +0200

    StartHere

---
+++ pca10028/s110/arm5_no_packs/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'nrf51-ble-tutorial-service' 
+ * Target:  'nrf51422_xxac_s110' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
