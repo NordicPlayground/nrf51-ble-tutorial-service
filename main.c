871568c6023203052655233f587220240b1a761a
Martin Børs-Lind <martin.bors-lind@nordicsemi.no>
2015-08-11 10:46:52 +0200

    Minor cleanup

--- main.c
+++ main.c
@@ -126,10 +126,6 @@ static void gap_params_init(void)
                                           strlen(DEVICE_NAME));
     APP_ERROR_CHECK(err_code);
 
-    /* OUR_JOB: Use an appearance value matching the application's use case.
-    err_code = sd_ble_gap_appearance_set(BLE_APPEARANCE_);
-    APP_ERROR_CHECK(err_code); */
-
     memset(&gap_conn_params, 0, sizeof(gap_conn_params));
 
     gap_conn_params.min_conn_interval = MIN_CONN_INTERVAL;
