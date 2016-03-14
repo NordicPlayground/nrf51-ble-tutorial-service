cb3a31e74a7d49381c079cba6b64bad9f19e00d9
badcoffee <mbl@test.com>
2016-03-14 12:58:52 +0100

    init commit

---
+++ SDK 9.0.0/README.md
@@ -0,0 +1,21 @@
+# nrf51-ble-tutorial-service
+This example is meant to be used togheter with the tutorial "BLE Services, a beginner's tutorial" found on https://devzone.nordicsemi.com/tutorials/8/ble-services-a-beginners-tutorial/. The purpose of the tutorial is to get you started with the basics of the nRF51x22 and Bluetooth Low Energy (BLE). More specifically this tutorial will go through the bare minimum of steps to get your first services up and running. The tutorial is fairly superficial and is meant to be a hands-on introduction to BLE services. It is intended to be a natural continuation of the tutorial "BLE Advertising, a beginner's tutorial".
+
+Requirements
+
+<ul><ul>
+<li>nRF51 DK Development Kit
+<li>nRF51 Dongle
+<li>Keil v5.12 or later
+<li>Master Control Panel (MCP) v3.9.0.6 (You can also use the Master Control Panel found in the nRF Toolbox app for Android or the LightBlue app for iPhone)
+<li>nRFgo Studio v1.20.02
+<li>SDK v9.0.0
+<li>SoftDevice S110 v8.0.0
+<li>Example files found on github.
+</ul></ul>
+
+Other kits, dongles and software versions might work as well, but this will not be covered here.
+
+To compile it, clone the repository to "your_SDK_v9.0.0_folder\examples\ble_peripheral".
+
+Please post any questions about this project on https://devzone.nordicsemi.com.
---
+++ SDK 9.0.0/our_service.c
@@ -0,0 +1,62 @@
+/* Copyright (c) Nordic Semiconductor ASA
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ *   1. Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ *   2. Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ * 
+ *   3. Neither the name of Nordic Semiconductor ASA nor the names of other
+ *   contributors to this software may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ * 
+ *   4. This software must only be used in a processor manufactured by Nordic
+ *   Semiconductor ASA, or in a processor manufactured by a third party that
+ *   is used in combination with a processor manufactured by Nordic Semiconductor.
+ * 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+
+#include <stdint.h>
+#include <string.h>
+#include "our_service.h"
+#include "ble_srv_common.h"
+#include "app_error.h"
+#include "SEGGER_RTT.h"
+
+/**@brief Function for initiating our new service.
+ *
+ * @param[in]   p_our_service        Our Service structure.
+ *
+ */
+void our_service_init(ble_os_t * p_our_service)
+{
+    uint32_t   err_code; // Variable to hold return codes from library and softdevice functions
+    
+    // OUR_JOB: Declare 16 bit service and 128 bit base UUIDs and add them to BLE stack table     
+    
+    // OUR_JOB: Add our service
+
+    // Print messages to Segger Real Time Terminal
+    // UNCOMMENT THE FOUR LINES BELOW AFTER INITIALIZING THE SERVICE OR THE EXAMPLE WILL NOT COMPILE.
+//    SEGGER_RTT_WriteString(0, "Exectuing our_service_init().\n"); // Print message to RTT to the application flow
+//    SEGGER_RTT_printf(0, "Service UUID: 0x%#04x\n", service_uuid.uuid); // Print service UUID should match definition BLE_UUID_OUR_SERVICE
+//    SEGGER_RTT_printf(0, "Service UUID type: 0x%#02x\n", service_uuid.type); // Print UUID type. Should match BLE_UUID_TYPE_VENDOR_BEGIN. Search for BLE_UUID_TYPES in ble_types.h for more info
+//    SEGGER_RTT_printf(0, "Service handle: 0x%#04x\n", p_our_service->service_handle); // Print out the service handle. Should match service handle shown in MCP under Attribute values
+}
